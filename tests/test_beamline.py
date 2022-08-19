2739590c53bc11f7e315601660be6bfa95b9f9b3
Malte Titze <mtitze@users.noreply.github.com>
2022-08-19 12:41:51 +0200

    test speedup

--- tests/test_beamline.py
+++ tests/test_beamline.py
@@ -7,11 +7,11 @@ from lieops.solver.yoshida import symint
 from accphys import beamline
 from accphys.elements import hard_edge_element
 
-@pytest.mark.parametrize("q0, p0, yoshida_order, n_slices, tol", 
+@pytest.mark.parametrize("xi0, eta0, yoshida_order, n_slices, tol", 
                          [(-0.0047, 0.0038, 0, 3, 1e-6), 
-                          (0.022, 0.015, 1, 4, 5e-4), 
-                          (-0.3, 0.53, 2, 12, 5e-3)])
-def test_splitting1(q0, p0, yoshida_order, n_slices, tol):
+                          (0.022, 0.015, 0, 4, 5e-4), 
+                          (-0.3, 0.53, 0, 12, 5e-3)])
+def test_splitting1(xi0, eta0, yoshida_order, n_slices, tol):
     '''
     Test if splitting a single element gives roughly the same values in tracking as if
     passing through the original element.
@@ -30,7 +30,7 @@ def test_splitting1(q0, p0, yoshida_order, n_slices, tol):
     bls = bl.split(keys=keys, scheme=yoshida_scheme, n_slices=n_slices)
     bls.calcOneTurnMap(method='heyoka')
     
-    result = bl(q0, p0)
-    result_s = bls(q0, p0)
+    result = bl(xi0, eta0)
+    result_s = bls(xi0, eta0)
     
     assert max(np.abs(result - result_s)) < tol
