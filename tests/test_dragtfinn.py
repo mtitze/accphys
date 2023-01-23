cb7912cde8e2954533560b6b846b3d4bf9521018
Malte Titze <mtitze@users.noreply.github.com>
2023-01-23 15:47:52 +0100

    tolerance in D/F test

--- tests/test_dragtfinn.py
+++ tests/test_dragtfinn.py
@@ -4,7 +4,7 @@ from lieops.core.tools import ad2poly, poly2ad
 
 from accphys import beamline
 
-def test_df1D_order1(tol1=5e-12, tol2=1e-14, max_power=10):
+def test_df1D_order1(tol1=5e-12, tol2=1e-14, tol_checks=1e-11, max_power=10):
     '''
     Test if the 2x2 BCH formula + Dragt/Finn factorization of a beamline will give back the same values.
     '''
@@ -19,8 +19,8 @@ def test_df1D_order1(tol1=5e-12, tol2=1e-14, max_power=10):
     C = ad2poly(bch_2x2(poly2ad(B), poly2ad(A)), max_power=max_power) # Note the exchange in the order of A and B: First the A-operator must be executed, then B.
     bl_C = beamline(C)
 
-    cdf1 = bl_C.dragtfinn(0, 0, order=1, power=20, comb2=False)
-    cdf2 = bl_C.dragtfinn(0, 0, order=1, method='2flow', comb2=False)
+    cdf1 = bl_C.dragtfinn(0, 0, order=1, power=20, comb2=False, tol_checks=tol_checks)
+    cdf2 = bl_C.dragtfinn(0, 0, order=1, method='2flow', comb2=False, tol_checks=tol_checks)
     
     for k in range(2):
         assert (cdf1[k].hamiltonian - bl[k].hamiltonian).above(tol1) == 0
