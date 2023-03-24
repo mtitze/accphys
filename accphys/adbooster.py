7fae903e50c68a5338f476c8d2b10a67a5cc0a1b
Malte Titze <mtitze@users.noreply.github.com>
2023-03-24 22:50:36 +0100

    adbooster working, but need more checks, cleanup and docs

--- accphys/adbooster.py
+++ accphys/adbooster.py
@@ -112,3 +112,76 @@ def _get_nested_patterns(rno_patterns, **kwargs):
         return _get_nested_patterns(rno_patterns_c, heap=heap)
     else:
         return heap
+    
+    
+def _first_ref_pattern_positions(npat):
+    '''
+    Get the positions of the patterns in the reference sequence, assuming that
+    the positions of the largest pattern is related to the reference sequence.
+    '''
+    lengths = sorted(npat.keys(), reverse=True)
+    if len(lengths) == 0:
+        return [], []
+    _pat_pos = lambda key: list(npat[key].items())[0] # for a given level (key), obtain the pattern and the position(s)
+    abs_position = _pat_pos(lengths[0])[1][0] # the position of the first occurence of the largest pattern
+    positions = [abs_position]
+    for length in lengths[1:]:
+        first_relative_position = _pat_pos(length)[1][0]
+        positions.append(first_relative_position + positions[-1])
+    return positions, lengths
+    
+    
+def _spaces_per_level(npat):
+    levels = sorted(npat.keys())
+    _pat_pos = lambda key: list(npat[key].items())[0] # for a given level (key), obtain the pattern and the position(s)
+    spaces_in_level = []
+    for level in levels:
+        pattern, pos = _pat_pos(level)
+        n_pos = len(pos) # number of occurences of pattern in next level
+        space = len(pattern)*n_pos # space pattern copies occupy in next level
+        for e in spaces_in_level:
+            if len(e) > 0:
+                sp, po = e[-1]
+                e.append((sp*n_pos, po*n_pos))
+        spaces_in_level.append([(space, n_pos)])
+    # the k-th entry ek of 'spaces_in_level' is a list of integers. It is related to level[k].
+    # ek[j] consists of two numbers:
+    # 1) The space occupied of level[k] in level[k + j + 1].
+    # 2) The total number of occurences of level[k] in level[k + j + 1]
+    return spaces_in_level
+
+    
+def _merge_descent(cyc):
+    '''
+    Merge a given chain in such a way that we merge through a sequence of large sub-patterns.
+    '''
+    npat = _get_nested_patterns(find_rno_patterns(cyc.ordering)[0])    
+    _pat_pos = lambda key: list(npat[key].items())[0]
+    abs_pos, levels = _first_ref_pattern_positions(npat)
+    levels = levels[::-1]
+    abs_pos = abs_pos[::-1]
+
+    spl = _spaces_per_level(npat)
+    
+    cycr = cyc
+    level_sizes = {k: len(_pat_pos(levels[k])[0]) for k in range(len(levels))}
+    for k in range(len(levels) - 1):
+        level = levels[k]
+        abs_pattern, positions = _pat_pos(level) # abs_pattern: current pattern, but relative to the original ordering
+        n_positions = len(positions) # the number of the current pattern occuring within the next pattern.
+        size = level_sizes[k]
+        pattern = tuple(cycr.ordering[abs_pos[k]: abs_pos[k] + size])
+        #print (size, pattern, positions[0], n_positions, abs_pos[k])
+        cycr = cycr.merge(pattern=pattern)
+        
+        # modify all sizes accordingly
+        space_k = spl[k]
+        for l in range(len(space_k) - 1):
+            space, n_occurences = space_k[l] # occupied space of the pattern in level k + l + 1 and occurences
+            level_sizes[k + l + 1] = level_sizes[k + l + 1] - n_occurences*size + n_occurences # "+ n_positions", because 'n_positions' new elements are inserted
+    return cycr
+    
+def pattern_cycle(*point, dchain, **kwargs):
+    cyc = dchain.cycle(*point, outf=None, **kwargs)
+    cycr = _merge_descent(cyc)
+    return cycr.compose()
